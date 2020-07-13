7e6489514ebb37b8ccdadda72797fa62651d54f5
HAEM <54823396+Haemin-Park@users.noreply.github.com>
2020-07-13 20:18:13 +0900

    UI 수정 및 코드정리

--- app/src/main/java/phm/example/project_Diary/FriendsList.java
+++ app/src/main/java/phm/example/project_Diary/FriendsList.java
@@ -2,13 +2,9 @@ package phm.example.project_Diary;
 
 public class FriendsList {
     private String fid;
-    //private String displayname;
-    //private String imageURL;
 
     public FriendsList(String fid) {
         this.fid = fid;
-        //this.displayname = displayname;
-        //this.imageURL = imageURL;
     }
     public FriendsList(){
 
@@ -21,23 +17,5 @@ public class FriendsList {
     public void setFid(String fid) {
         this.fid = fid;
     }
-/*
-    public String getDisplayname() {
-        return displayname;
-    }
-
-    public void setDisplayame(String username) {
-        this.displayname = username;
-    }
-
-    public String getImageURL() {
-        return imageURL;
-    }
-
-    public void setImageURL(String imageURL) {
-        this.imageURL = imageURL;
-    }
-
- */
 
 }
